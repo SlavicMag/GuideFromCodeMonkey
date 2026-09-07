ce4aa0a6e016cd19896ef59086646002162f4c6a
SlavicMag <govoruxadaniil@gmail.com>
2026-09-08 00:23:33 +0300

    Добавил новые способы взять продукты
    Также добавлены значки поверх тарелки с продуктами

--- GuideFromCodeMonky/Assets/Scripts/KitchenObject.cs
+++ GuideFromCodeMonky/Assets/Scripts/KitchenObject.cs
@@ -13,8 +13,6 @@ public class KitchenObject : MonoBehaviour
         return kitchenObjectSO;
     }
 
-
-
     public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent)
     {
         if(this.kitchenObjectParent != null)
@@ -45,6 +43,20 @@ public class KitchenObject : MonoBehaviour
         kitchenObjectParent.ClearKitchenObject();
 
         Destroy(gameObject);
+    }
+
+    public bool TryGetPlate(out PlateKitchenObject plateKitchenObject)
+    {
+        if (this is PlateKitchenObject)
+        {
+            plateKitchenObject = this as PlateKitchenObject;
+            return true;
+        }
+        else
+        {
+            plateKitchenObject = null;
+            return false;
+        }
     }
 
     public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
